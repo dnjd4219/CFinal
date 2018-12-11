9379f72a969b41b889db7e42175516e343d39cbb
SHyoJun <42951642+SHyoJun@users.noreply.github.com>
2018-12-11 21:34:30 +0900

    Add files via upload
    ㄴㄴ

---
+++ chap10/10-2.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+int gcd( int a, int b );
+int lcm( int a, int b );
+
+int main( )
+{
+	int su1, su2;
+	printf( "두개의 정수 입력 : " );
+	scanf( "%d%d", &su1, &su2 );
+	printf( "GCD( 최대공약수 ): %d\n", gcd( su1, su2 ) );
+	printf( "LCM( 최소공배수 ): %d\n\n", lcm( su1, su2 ) );
+	return 0;
+}
+
+int gcd( int a, int b )
+{
+	if( a < b )
+	{
+		return gcd( b, a );
+	}
+	else if( a % b == 0 )
+	{
+		return b;
+	}
+	else
+	{
+		return gcd( b, a%b );
+	}
+}
+
+int lcm( int a, int b )
+{
+	return a * b / gcd( a, b );
+}
